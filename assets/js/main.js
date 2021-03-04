1680c7f953208cb430504a39edb25331e7d61a9e
emanuele <emattina97@gmail.com>
2021-03-04 15:38:29 +0100

    Carico esercizio completo

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+// chiedere all'utente il cognome
+// inserirlo in un array con altri cognomi
+// stampare l'array ordinato alfabeticament
+// scrivere la posizione nella lista in cui l'utente si trova
+
+var lista = ["Bianchi", "Rossi", "Perlari", "Lodigiani", "Mastrobattista", "Verdi"];
+var cognomeUtente;
+var posizione;
+var i;
+
+cognomeUtente = prompt("Inserisci il tuo cognome");
+
+console.log(cognomeUtente[0].toUpperCase() + cognomeUtente.slice(1));
+lista.push(cognomeUtente[0].toUpperCase() + cognomeUtente.slice(1));
+
+lista.sort();
+
+for (i = 0; i < lista.length; i++)
+{
+  document.getElementById('lista').innerHTML += "<li>" + lista[i] + "</li>";
+  if (cognomeUtente == lista[i])
+  {
+    posizione = i;
+  }
+}
+
+document.getElementById('posizione').innerHTML += "Ti trovi alla posizione numero " + (posizione + 1);
